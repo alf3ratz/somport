0970015e6aecddf98293286dd9721866df36d9f8
a.petropavlovskiy <a.petropavlovskiy@tinkoff.ru>
2024-09-10 22:19:36 +0300

    first

---
+++ src/main/java/com/hse/somport/somport/SomportApplication.java
@@ -0,0 +1,13 @@
+package com.hse.somport.somport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SomportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hse/somport/somport/SomportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hse.somport.somport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
